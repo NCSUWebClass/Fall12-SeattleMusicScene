d436872c9935175f3768d351d0a314ee11306b03
Web Class Team <rafredet@ncsu.edu>
2012-12-02 16:01:55 -0500

    added form to start testing search box

--- index.php
+++ index.php
@@ -27,10 +27,12 @@
 			
 			<h1 class="center-text">Welcome to the Seattle Band Map</h1>
 			<br>
+			<form action="search.php" method="post">
 			<input type="search" name="search-box" id="search-box" placeholder="Artist or Band Name" data-theme="a" />
 			<div class="center-text">
-			<input type="submit" data-theme="a" value="Search" data-inline="true"/>
+			<input type="submit" name="Submit" data-theme="a" value="Submit" data-inline="true"/>
 			</div>
+			</form>
 			<br>
 			<br>
 
